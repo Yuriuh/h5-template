e10fe14668505f3860cd1dec4319ef67c9ec3d33
Yuriuh <642192014@qq.com>
2020-11-07 23:53:42 +0800

    添加 axios

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,27 @@
 const autoprefixer = require('autoprefixer')
 const pxtorem = require('postcss-pxtorem')
 const tailwindcss = require('tailwindcss')
+const path = require('path')
+
+const resolve = path => {
+  return path.resolve(__dirname, path)
+}
 
 module.exports = {
   outputDir: 'dist',
   publicPath: process.env.NODE_ENV === 'production' ? '/h5-tempate/' : '/',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        public: resolve('public'),
+        src: resolve('src'),
+        components: resolve('src/components'),
+        service: resolve('src/services'),
+        utils: resolve('src/utils'),
+        store: resolve('src/store'),
+      },
+    },
+  },
   css: {
     loaderOptions: {
       postcss: {
