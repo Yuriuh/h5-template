74d7a98e43bfb62ae4ffe5bbc3db0ad6cfe3c779
Yuriuh <642192014@qq.com>
2020-11-12 23:12:18 +0800

    添加视频组件

--- src/views/About.vue
+++ src/views/About.vue
@@ -9,7 +9,7 @@
       <span>center</span>
       <span>right</span>
     </div>
-    <div class="p-1">
+    <!-- <div class="p-1">
       <fetch-data
         :url="url"
         :toastLoading="true"
@@ -20,17 +20,23 @@
           <div>{{ data }}</div>
         </div>
       </fetch-data>
+    </div> -->
+
+    <div class="flex px-1">
+      <div id="mse"></div>
     </div>
   </div>
 </template>
 
 <script>
 import { Button, Toast } from 'vant'
-import FetchData from '@/components/FetchData'
+// import FetchData from '@/components/FetchData'
+import Player from 'xgplayer'
+import video from '../assets/test.mp4'
 export default {
   components: {
     [Button.name]: Button,
-    FetchData,
+    // FetchData,
   },
   data() {
     return {
@@ -38,7 +44,30 @@ export default {
       url: 'https://jsonplaceholder.typicode.com/posts/1',
     }
   },
+  mounted() {
+    let player = new Player({
+      id: 'mse',
+      url: video,
+      fluid: true,
+      autoplay: true,
+      loop: true,
+      fitVideoSize: 'auto',
+      ignores: ['time', 'progress'],
+    })
+  },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+#mse {
+  flex: auto;
+  border-radius: 12px;
+  > video {
+    border-radius: 12px;
+  }
+  > .xgplayer-controls {
+    border-bottom-left-radius: 12px;
+    border-bottom-right-radius: 12px;
+  }
+}
+</style>
