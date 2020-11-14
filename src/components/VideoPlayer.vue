ec514d4b012fc34444c46ec77f0d955162fa1b53
Yuriuh <642192014@qq.com>
2020-11-14 20:25:29 +0800

    封装 video 组件

---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,37 @@
+<template>
+  <div :id="id"></div>
+</template>
+
+<script>
+// 引入 EventBus
+export default {
+  props: {
+    id: {
+      type: String,
+    },
+    url: {
+      type: String,
+    },
+  },
+  mounted() {
+    let player = new Player({
+      id: this.id,
+      url: this.url,
+      fluid: true,
+      autoplay: true,
+      loop: true,
+      ignores: ['time', 'progress'],
+      height: 240,
+      muted: true,
+      closeVideoPreventDefault: true,
+      closeVideoStopPropagation: true,
+      closeVideoClick: true,
+      closeVideoTouch: true,
+    })
+    // 监听事件
+  },
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped></style>
