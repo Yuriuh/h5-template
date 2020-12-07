de16af6599953560a799437fa846526bf8bcaece
Yuriuh <642192014@qq.com>
2020-12-07 22:54:04 +0800

    增加 drag demo

--- src/views/Directives.vue
+++ src/views/Directives.vue
@@ -12,6 +12,15 @@
         </van-button>
       </div>
     </div>
+
+    <div class="p-1">
+      <h3>2. Dragable</h3>
+      <div class="p-1">
+        <van-button type="primary" size="mini" v-drag>
+          drag
+        </van-button>
+      </div>
+    </div>
   </div>
 </template>
 
