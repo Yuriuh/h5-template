f59a6636458fcedf4b0350b2173e4b943bca88db
Yuriuh <642192014@qq.com>
2021-04-23 02:20:15 +0800

    更新依赖

--- src/components/CompA.vue
+++ src/components/CompA.vue
@@ -1,18 +1,18 @@
 <template>
-    <div>Component A</div>
+  <div>Component A</div>
 </template>
 
 <script>
-import { mapMutations } from 'vuex';
-import { sleep } from '../utils/sleep';
+import { mapMutations } from 'vuex'
+import { sleep } from '../utils/sleep'
 export default {
-    async mounted() {
-        this.INCREASE_LOADING();
-        await sleep(1000);
-        this.DECREASE_LOADING();
-    },
-    methods: {
-        ...mapMutations('loading', ['INCREASE_LOADING', 'DECREASE_LOADING']),
-    },
-};
+  async mounted() {
+    this.INCREASE_LOADING()
+    await sleep(1000)
+    this.DECREASE_LOADING()
+  },
+  methods: {
+    ...mapMutations('loading', ['INCREASE_LOADING', 'DECREASE_LOADING']),
+  },
+}
 </script>
--- src/components/CompB.vue
+++ src/components/CompB.vue
@@ -1,18 +1,18 @@
 <template>
-    <div>Component B</div>
+  <div>Component B</div>
 </template>
 
 <script>
-import { mapMutations } from 'vuex';
-import { sleep } from '../utils/sleep';
+import { mapMutations } from 'vuex'
+import { sleep } from '../utils/sleep'
 export default {
-    async mounted() {
-        this.INCREASE_LOADING();
-        await sleep(2000);
-        this.DECREASE_LOADING();
-    },
-    methods: {
-        ...mapMutations('loading', ['INCREASE_LOADING', 'DECREASE_LOADING']),
-    },
-};
+  async mounted() {
+    this.INCREASE_LOADING()
+    await sleep(2000)
+    this.DECREASE_LOADING()
+  },
+  methods: {
+    ...mapMutations('loading', ['INCREASE_LOADING', 'DECREASE_LOADING']),
+  },
+}
 </script>
